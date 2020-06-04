6f9949eff7e8ef3550267abed4f0b38641a81099
Jefferson Adami <jefferson.adami@gmail.com>
2020-06-03 23:05:40 -0300

    Implementação server.

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,9 @@
-import express from 'express';
+import express, { response } from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-  console.log('Listagem de usuários');
-
-  response.json(['Jose', 'Elza', 'Reinaldo', 'Jefferson']);
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
